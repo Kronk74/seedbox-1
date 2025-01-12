9aec80297ead588a2af34a49c1b3dbdca9e26310
Kronk74 <ramon.thomas.74@gmail.com>
2025-01-12 22:39:58 +0100

    feat: Move requirements checks to check_utilities function

--- run-seedbox.sh
+++ run-seedbox.sh
@@ -11,37 +11,6 @@ check_utilities
 SKIP_PULL=0
 DEBUG=0
 
-echo "[$0] ***** Checking required tools... *****"
-REQUIRED_TOOLS=("docker" "docker-compose" "yq" "jq")
-
-for tool in "${REQUIRED_TOOLS[@]}"; do
-  if ! command -v $tool &> /dev/null; then
-    echo "[$0] ERROR: $tool is not installed. Please install $tool and try again."
-    exit 1
-  fi
-done
-
-# Check if versions of required tools are correct
-check_version() {
-  local tool=$1
-  local required_version=$2
-  local current_version=$($tool --version | grep -oP '\d+\.\d+')
-  if [[ $(echo -e "$current_version\n$required_version" | sort -V | head -n1) != "$required_version" ]]; then
-    echo "[$0] ERROR: $tool version $required_version or higher is required. Current version is $current_version."
-    exit 1
-  fi
-}
-
-# Define required versions for each tool
-declare -A REQUIRED_VERSIONS
-REQUIRED_VERSIONS=( ["docker"]="20.10" ["docker-compose"]="1.29" ["yq"]="4.6" ["jq"]="1.6" )
-
-# Check versions
-echo "[$0] ***** Checking version of required tools... *****"
-for tool in "${!REQUIRED_VERSIONS[@]}"; do
-  check_version $tool ${REQUIRED_VERSIONS[$tool]}
-done
-
 for i in "$@"; do
   case $i in
     --no-pull)
@@ -550,7 +519,7 @@ rm -f .env.concat
 
 echo "[$0] ***** Clean unused images and volumes... *****"
 docker image prune -af
-docker volume prune  -f
+docker volume prune -f
 
 echo "[$0] ***** Done! *****"
 exit 0
--- tools/tools.sh
+++ tools/tools.sh
@@ -5,19 +5,34 @@
 ##############################################################################
 
 check_utilities () {
-  # Check that jq is installed
-  if ! which jq >/dev/null; then
-    echo "[$0] jq does not exist. Install it from here: https://stedolan.github.io/jq/download/"
-    echo "[$0] Please install jq version 1.5 or above."
-    echo "[$0] Also, please make sure it is in the PATH."
-    exit 1
-  fi
+  echo "[$0] ***** Checking required tools... *****"
+  REQUIRED_TOOLS=("docker" "docker compose" "yq" "jq")
 
-  # Check that yq is installed
-  if ! which yq >/dev/null; then
-    echo "[$0] yq does not exist. Install it from here: https://github.com/mikefarah/yq/releases"
-    echo "[$0] Please install yq version 4 or above."
-    echo "[$0] Also, please make sure it is in the PATH."
-    exit 1
-  fi
+  for tool in "${REQUIRED_TOOLS[@]}"; do
+    if ! command -v $tool &> /dev/null; then
+      echo "[$0] ERROR: $tool is not installed. Please install $tool and try again."
+      exit 1
+    fi
+  done
+
+  # Check if versions of required tools are correct
+  check_version() {
+    local tool=$1
+    local required_version=$2
+    local current_version=$($tool --version | grep -oP '\d+\.\d+')
+    if [[ $(echo -e "$current_version\n$required_version" | sort -V | head -n1) != "$required_version" ]]; then
+      echo "[$0] ERROR: $tool version $required_version or higher is required. Current version is $current_version."
+      exit 1
+    fi
+  }
+
+  # Define required versions for each tool
+  declare -A REQUIRED_VERSIONS
+  REQUIRED_VERSIONS=( ["docker"]="20.10" ["docker-compose"]="1.29" ["yq"]="4.6" ["jq"]="1.6" )
+
+  # Check versions
+  echo "[$0] ***** Checking version of required tools... *****"
+  for tool in "${!REQUIRED_VERSIONS[@]}"; do
+    check_version $tool ${REQUIRED_VERSIONS[$tool]}
+  done
 }
